d68c248d4b36e6d767ae6d153c6475670ee7f8c1
rsavoj <97634110+rsavoj@users.noreply.github.com>
2022-01-19 16:49:08 -0800

    Changes made to MarkdownParse.java so that the file does not add images to the array

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,17 +11,16 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-           
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-          
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-          
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            System.out.println("The value of current index " + currentIndex);
+            if (!(markdown.charAt(nextOpenBracket-1)== '!')){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            
             currentIndex = closeParen + 1;
-            System.out.println("The value of current index " + currentIndex);
         }
         return toReturn;
     }
