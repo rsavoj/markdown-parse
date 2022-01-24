b0095304b7b1c467289ba31bee32fcda668d7f1b
rsavoj <97634110+rsavoj@users.noreply.github.com>
2022-01-24 15:35:29 -0800

    If statement added to fix the index out of bounds error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,9 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if(openParen ==-1 || closeParen == -1 ){
+                break;
+            }
             if (!(markdown.charAt(nextOpenBracket-1)== '!')){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
