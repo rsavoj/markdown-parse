3f9c127c90a9f45509241118f2414b8c8d54f163
rsavoj <97634110+rsavoj@users.noreply.github.com>
2022-03-09 15:30:19 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -25,14 +25,15 @@ public class MarkdownParse {
             if(openParen ==-1 || closeParen == -1 ){
                break;
             }
-            if (nextOpenBracket>0  && !(markdown.charAt(nextOpenBracket-1)== '!' ) && openParen-nextCloseBracket == 1){
+            if (nextOpenBracket>0  && !(markdown.charAt(nextOpenBracket-1)== '!' ) 
+            && openParen-nextCloseBracket == 1){
                 System.out.println("execute");
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             else if (nextOpenBracket == 0 && openParen-nextCloseBracket == 1){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
-            System.out.println("The openParen " + openParen +": the closed Paren " + closeParen + "next closed bra " + nextCloseBracket +" "+ nextOpenBracket);
+           
             currentIndex = closeParen + 1;
         }
         return toReturn;
@@ -42,6 +43,6 @@ public class MarkdownParse {
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
-      //  System.out.println("We are timing how long it takes to run all our files it took 1 milute and 15 seconds");
+     
     }
 }
